5079159e2043c7b1351e79f07fbd9e7454270d27
tusharshaha <tusharshaha128@gmail.com>
2024-11-10 21:31:48 +0600

    created model with prisma

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
 import express from "express";
-
-require("dotenv").config();
-require("colors");
+import "dotenv/config";
+import "colors";
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: any) => {
   res.send("Server Running");
-})
-
+});
 
 const server = app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}/`.bgYellow);
-})
+});
 
 // handle globaly unhandle Rejection
 process.on("unhandledRejection", (error: Error) => {
   server.close(() => {
-      process.exit(1);
+    process.exit(1);
   });
 });
-
